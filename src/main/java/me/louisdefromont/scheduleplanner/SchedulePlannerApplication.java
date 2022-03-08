b9024ecf015d4cfad35828a8d7b0149ac45ce123
louisdefromont <louis.defromont@gmail.com>
2022-03-08 18:42:35 -0500

    Enable Entity Scan

--- src/main/java/me/louisdefromont/scheduleplanner/SchedulePlannerApplication.java
+++ src/main/java/me/louisdefromont/scheduleplanner/SchedulePlannerApplication.java
@@ -2,8 +2,14 @@ package me.louisdefromont.scheduleplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("me.louisdefromont")
+@ComponentScan(basePackages = { "me.louisdefromont" })
+@EntityScan("me.louisdefromont")
 public class SchedulePlannerApplication {
 
 	public static void main(String[] args) {
